5c39881fe8902ab6630a41faf91cbb12753f7ff8
Eldwick <jamesjalandoni1@gmail.com>
2014-09-25 12:50:41 -0400

    Removed sensitive info

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 var hipchat = require('node-hipchat');
 
-var HC = new hipchat('4e9f38bc09830a798a3e091c13fa20');
+var HC = new hipchat('API-KEY');
 
 router.post('/sendChat', function(req, res) {
   var params = {
-    room: 846262, // Found in the JSON response from the call above
+    room: #, // Found in the JSON response from the call above
     from: req.body.nickName,
     message: req.body.message + ' <img src="'+req.body.img+'">',
     color: req.body.color
